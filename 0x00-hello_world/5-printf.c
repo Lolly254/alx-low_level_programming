731400f08aa17338d628dfa232a137b58a9d8b9b
Lolly254 <mukabi54@gmail.com>
2022-03-10 17:39:57 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,7 @@
 /*Main - Entry point*/
 /* Return Always 0 (success)*/
 int main(void)
+/*This is the main function*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
