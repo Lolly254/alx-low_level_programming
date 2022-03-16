d91a2abbc04bc4d91549a1c262658d8a47c52f86
Lolly254 <mukabi54@gmail.com>
2022-03-16 18:52:30 +0000

    sign of numbers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,18 +11,17 @@
  */
 int print_sign(int n)
 {
-int k;
-if (k > 0)
+if (n > 0)
 {
 _putchar(43);
 return (1);
 }
-else if (k < 0)
+else if (n < 0)
 {
 _putchar(45);
 return (-1);
 }
-else if (k == 0)
+else if (n == 0)
 {
 _putchar(48);
 return (0);
