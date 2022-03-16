291575454a05802615b0ff61b72c1347572753e8
Lolly254 <mukabi54@gmail.com>
2022-03-16 19:10:31 +0000

    absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main'h"
+
+/**
+ * _abs - gives the absolute value
+ * of an integer.
+ *
+ * @k: input number as an integer.
+ *
+ * Return: absolute value
+ *
+ */
+int _abs(int k)
+{
+int k;
+if (k >= 0)
+{
+return (k);
+}
+else
+{
+return (k * -1);
+}
+}
+
