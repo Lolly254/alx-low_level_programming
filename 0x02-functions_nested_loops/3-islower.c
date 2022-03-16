eaf9967bd148d8cd10f8092c966641a4b8f1c5ed
Lolly254 <mukabi54@gmail.com>
2022-03-16 16:58:38 +0000

    if lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check if the alphabet is small
+ * using the ascii table
+ * Return: 1 if the character is small and 0 if not
+ */
+int _islower(int c);
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+putchar('\n');
+}
+
