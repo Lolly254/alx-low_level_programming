8dd3d9a1f7ed0a59d1272eb60acb027af16beee9
Lolly254 <mukabi54@gmail.com>
2022-03-18 21:43:50 +0000

    main 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - to test te function prints
+ * negative or positive integer
+ * Return:0
+ */
+int main(void)
+{
+int i;
+int i = 0;
+positive_or_negative(i);
+return (0);
+}
