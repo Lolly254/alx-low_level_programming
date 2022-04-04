f66691bff91bae58db150f703fc76495d4101b45
Lolly254 <mukabi54@gmail.com>
2022-04-04 10:42:11 +0000

    program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 /**
- * num-checker - checks if a given character is a number or not
- * @a: char to be checked 
+ * num_checker - checks if a given char is a number or not
+ * @a: char to be checked
  * Return: 1 if it is a number, else 0
  */
 int num_checker(char *a)
@@ -12,6 +12,7 @@ int num_checker(char *a)
 	int i = 0;
 	int num = 0;
 	int len = strlen(a);
+
 	while (i < len)
 	{
 		if (a[i] < '0' && a[i] > '9')
@@ -27,20 +28,21 @@ int num_checker(char *a)
 	return (num);
 }
 /**
- * main - add positive numbers
+ * main - adds two positive numbers
  * @argc: argument count
  * @argv: array of arguments strings
- * Retrun: result of addition or 1
+ * Return: result of addition or 1
  */
 int main(int argc, char *argv[])
 {
 	int i, num, sum = 0;
+
 	for (i = 1; i < argc; i++)
 	{
-		 num = num_checker(argv[i]);
-		 if (num == -1)
-		 {
-		 	printf("Error\n");
+		num = num_checker(argv[i]);
+		if (num == -1)
+		{
+			printf("Error\n");
 			return (1);
 		}
 		sum += num;
