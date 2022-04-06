b1a2e5d2b41c628d0c6371014d837432a16b419e
Lolly254 <mukabi54@gmail.com>
2022-04-06 20:01:54 +0000

    static lib

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string of characters
+ * @c: character to be located
+ *
+ * Return: pointer to the first occurance of c
+ * else NULL
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+
+for (i = 0; s[i] != '\0' ; i++)
+{
+if (s[i] == c)
+return (&s[i]);
+}
+return (0);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string from where subtring
+ * have to be located
+ * @needle: substring withing the haystack
+ *
+ * Return: pointer at the begining if string found
+ * else return null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int k;
+	int j;
+
+	for (k = 0; haystack[k] != '\0'; k++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[k + j] != needle[j])
+			{
+				break;
+			}
+			if (needle[j])
+			{
+			return (&haystack[k]);
+			}
+		}
+	}
+	return (NULL);
+}
