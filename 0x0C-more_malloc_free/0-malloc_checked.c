7ca69e38876770d83d3a02e8129913f608c871db
Lolly254 <mukabi54@gmail.com>
2022-04-08 20:17:52 +0000

    size allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates space in the memory
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+	p = malloc(sizeof(unsigned int) * b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
