92eaca2f83b64add070a894282099c90bed492f2
Lolly254 <mukabi54@gmail.com>
2022-04-21 21:27:30 +0000

    elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,6 +8,7 @@
  * print_list - function that prints all elements of a list
  * size_t the list to be printed
  * if the str is empty print [0]
+ * @h: - pointer to elements of a list
  *
  * Return: the number of nodes
  */
@@ -15,9 +16,9 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while(h)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)");
 		else
 			printf("[%u] %s\n", h->len, h->str);
