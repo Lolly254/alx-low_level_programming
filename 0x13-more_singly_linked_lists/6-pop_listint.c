2e25adb85858ae071189e97e5b457311dcc46807
Lolly254 <mukabi54@gmail.com>
2022-04-26 19:38:06 +0000

    links are super cool->just deleted head node and stored its data somewhere else

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function that deletes the head node of a listint_t
+ * and returns the head nodes data(n)
+ * we are using a struct defined in the lists file
+ * @head: is the pointer to the fist node in the list
+ *
+ * Return: 0 if the list is empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	if (temp == NULL)
+		return (0);
+	int k = (*head)->n;
+
+	*head = (*head)->next;
+	free (temp);
+
+	return (k);
+}
+
