e496de6c9128ce84805e1307f49b6b473f5063ac
Lolly254 <mukabi54@gmail.com>
2022-04-27 19:49:04 +0000

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t k = 0;
-
-	while (h)
+	size_t j = 0;
+	
+	while(h)
 	{
 		printf("%d", h->n);
-		k++;
+		j++;
 		h = h->next;
 	}
-	return (k);
+	return (j);
 }
-
