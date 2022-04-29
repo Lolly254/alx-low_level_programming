4074bb7a4d7fd5880064450dc0a2901047b71ff6
Lolly254 <mukabi54@gmail.com>
2022-04-29 18:38:44 +0000

    print binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,17 +10,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int dec_val = 0;
+	int slen = strlen(b);
+	int total = 0;
+	int decval = 1;
 
-if (!b)
-return (0);
+	if (b == NULL)
+		return (0);
+	int i = (slen - 1);
 
-for (i = 0; b[i]; i++)
-{
-if (b[i] < '0' || b[i] > '1')
-return (0);
-dec_val = 2 * dec_val + (b[i] - '0');
-}
-return (dec_val);
+	while (i >= 0)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		if (b[i] == '1')
+		{
+			total += decval;
+		}
+		i--;
+		decval *= 2;
+	}
+	return (total);
 }
