5c8b2c93e689ee3232fe63b2c1fc46ae972d271f
Lolly254 <mukabi54@gmail.com>
2022-06-09 21:12:35 +0000

    hello C

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	if (h == 0)
-		return (count);
+	if (!h)
+		return (0);
 
 
-	while (h != NULL)
+	while (h)
 	{
 		printf(" %d\n ", h->n);
 		count++;
