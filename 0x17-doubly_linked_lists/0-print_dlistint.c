a9905a605ab8a3d6623d3456c437684d47b03465
Lolly254 <mukabi54@gmail.com>
2022-06-09 21:03:07 +0000

    hello c print elemetns of a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,10 +10,10 @@ size_t print_dlistint(const dlistint_t *h)
 	
 	size_t count = 0;
 	if(h == 0)
-		return (size);
+		return (count);
 
 
-	while (temp != NULL)
+	while (h != NULL)
 	{
 		printf(" %d\n ", h->n);
 		count++;
